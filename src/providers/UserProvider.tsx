2fa2ab17f6fd0d611481b607a4ade6d399d043b0
Roman Nebel <r@nebel.im>
2025-10-09 17:53:54 +0200

    refactor: Add token check in UserProvider to redirect unauthenticated users to login

--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -1,8 +1,24 @@
-import { useState, ReactNode } from "react";
+import { useState, ReactNode, useEffect } from "react";
 import { User, UserContext } from "../context/UserContext";
+import { userStorage } from "../utils/storage";
+
+function redirectToLogin() {
+  window.location.replace("/login");
+}
 
 export function UserProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
+
+  function checkUserToken() {
+    if (!userStorage.hasAccessToken()) {
+      setUser(null);
+      redirectToLogin();
+    }
+  }
+
+  useEffect(() => {
+    checkUserToken();
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
