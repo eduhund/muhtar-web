74545946d9636e48ab496cd6797d03135af6f673
Roman Nebel <r@nebel.im>
2025-10-10 23:02:11 +0200

    refactor: Replace App.css with App.scss for consistent styling and update Login page dimensions for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 
 import { ConfigProvider } from "antd";
-import ruRU from "antd/lib/locale/ru_RU";
+import enUS from "antd/lib/locale/en_US";
 
 import { Summary } from "./pages";
 import Login from "./pages/Login/Login";
 
-import "./App.css";
+import "./App.scss";
 import { UserProvider } from "./providers/UserProvider";
 import { MembershipProvider } from "./providers/MembershipProvider";
 
 const App = () => {
   return (
     <ConfigProvider
-      locale={ruRU}
+      locale={enUS}
       theme={{ token: { colorPrimary: "#f04f23" } }}
     >
       <Routes>
