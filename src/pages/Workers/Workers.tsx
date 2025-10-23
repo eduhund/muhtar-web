a871948398796aaa9edee09e8b6e15607089034a
Roman Nebel <r@nebel.im>
2025-10-23 14:28:00 +0200

    feat: Add custom tooltip for bar chart in Workers component and refactor chart rendering

--- src/pages/Workers/Workers.tsx
+++ src/pages/Workers/Workers.tsx
@@ -8,7 +8,7 @@ import { Membership, TimetableItem } from "../../context/AppContext";
 import { useTimetable } from "../../hooks/useTimetable";
 import { useMembership } from "../../hooks/useMembership";
 import { Navigate } from "react-router-dom";
-import { BarChart, Bar } from "recharts";
+import { BarChart, Bar, Tooltip, TooltipContentProps, YAxis } from "recharts";
 
 import "./Workers.scss";
 import { useProjects } from "../../hooks/useProjects";
@@ -64,7 +64,11 @@ function getLast5DaysSummary(
       .reduce((acc, e) => acc + (e.duration || 0), 0);
     result.unshift({ date, duration });
   }
-  return result;
+  const formattedResult = result.map((item) => ({
+    date: dayjs(item.date).format("D MMMM"),
+    duration: item.duration / 60, // in hours
+  }));
+  return formattedResult;
 }
 
 function WorkerRow({ membership }: { membership: Membership }) {
@@ -92,6 +96,49 @@ function WorkerRow({ membership }: { membership: Membership }) {
 
   const lastFiveDaysData = getLast5DaysSummary(thisWeekEntries);
 
+  const CustomTooltip = ({
+    active,
+    payload,
+  }: TooltipContentProps<string | number, string>) => {
+    const isVisible = active && payload && payload.length;
+    return (
+      <div
+        className="WorkerRow-chart-tooltip"
+        style={{ visibility: isVisible ? "visible" : "hidden" }}
+      >
+        {isVisible && (
+          <>
+            <p className="label">{payload[0].payload.date}</p>
+            <p className="value">
+              Spent <strong>{payload[0].payload.duration}</strong>
+            </p>
+          </>
+        )}
+      </div>
+    );
+  };
+
+  const TinyBarChart = () => {
+    return (
+      <BarChart
+        style={{
+          width: "150px",
+          maxHeight: "100px",
+        }}
+        responsive
+        data={lastFiveDaysData}
+      >
+        <YAxis domain={[0, 8]} hide={true} />
+        <Tooltip content={CustomTooltip} cursor={false} />
+        <Bar
+          dataKey="duration"
+          fill="#f04f23"
+          background={{ fill: "rgba(0, 0, 0, 0.04)" }}
+        />
+      </BarChart>
+    );
+  };
+
   return (
     <div className="WorkerRow">
       <div className="WorkerRow-headline">
@@ -104,16 +151,7 @@ function WorkerRow({ membership }: { membership: Membership }) {
         </Paragraph>
       </div>
       <div className="WorkerRow-params">
-        <BarChart
-          style={{
-            maxWidth: "300px",
-            maxHeight: "100px",
-          }}
-          responsive
-          data={lastFiveDaysData}
-        >
-          <Bar dataKey="duration" fill="#f04f23" />
-        </BarChart>
+        <TinyBarChart />
         <Statistic
           title="Spent this week"
           value={totalWeekDuration.toFixed(0)}
